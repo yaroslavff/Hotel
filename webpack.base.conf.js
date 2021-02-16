13dddb4d6740aa8df8c087ad0eb522ae39be3ee1
yaroslavff <apreylg@yandex.ru>
2021-02-16 21:47:27 +0300

    Базовая разметка Webpack + подготовка структуры для верстки UI kit

---
+++ webpack.base.conf.js
@@ -0,0 +1,110 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = {
+  src: path.join(__dirname, './src'),
+  dist: path.join(__dirname, './dist')
+}
+
+module.exports = {
+
+  externals: {
+    paths: PATHS
+  },
+  entry: {
+    app: PATHS.src
+  },
+
+ output: {
+    filename: '[name].js',
+    path: PATHS.dist,
+    publicPath: '/'
+  },
+  module: {
+    rules: [{
+      test: /\.pug$/,
+      loader: 'pug-loader'
+    }, {
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: '/node_modules/'
+    }, {
+        test: /\.(svg|eot|ttf|woff|woff2)$/,
+        loader: 'url-loader',
+        options: {
+          name: '[name].[ext]'
+        }
+
+    }, {
+      test: /\.(png|jpg|gif|svg|)$/,
+      loader: 'file-loader',
+      options: {
+        name: '${PATHS.dist}/fonts/[name].[ext]'
+      }
+    }, {
+      test: /\.scss$/,
+      use: [
+        "style-loader",
+        {
+          loader: MiniCssExtractPlugin.loader,
+          options: {
+            publicPath: path.resolve(__dirname, 'dist') 
+          },
+        },
+        {
+          loader: "css-loader",
+          options: { sourceMap: true }
+        }, {
+          loader: "postcss-loader",
+          options: { sourceMap: true, postcssOptions: { path: './postcss.config.js' } }
+        }, {
+          loader: "sass-loader",
+          options: { sourceMap: true }
+        }
+      ]
+    }, {
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        {
+          loader: MiniCssExtractPlugin.loader,
+          options: {
+            publicPath: path.resolve(__dirname, 'dist') 
+          },
+        },
+        {
+          loader: 'css-loader',
+          options: { sourceMap: true }
+        }, {
+          loader: "postcss-loader",
+          options: { sourceMap: true, postcssOptions: { path: './postcss.config.js' } }
+        }
+      ]
+    }]
+  },
+  resolve: {
+    alias: {
+      '~': 'src',
+    }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css"
+    }),
+    new HtmlWebpackPlugin({
+      hash: false,
+      template: `${PATHS.src}/index.pug`,
+      filename: './index.html',
+      inject: true
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: `${PATHS.src}/img`, to: `${PATHS.dist}/img` },
+        { from: `${PATHS.src}/fonts`, to: `${PATHS.dist}/fonts` },
+        { from: `${PATHS.src}/static`, to: '' }
+      ]
+    })
+  ],
+}
