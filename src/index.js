e74e21d8049cc244a3d1207eb59b01cc08681f77
yaroslavff <apreylg@yandex.ru>
2021-03-05 00:52:07 +0300

    Блок form заменен на inputs, немного исправлена семантика

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "jquery";
 import "./interface/libs/jquery.maskedinput.min";
 
-import "./interface/form_elements/form";
+import "./interface/form_elements/inputs/input";
 
 
 // import "./components/common";
