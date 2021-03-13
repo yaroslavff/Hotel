2edc05326089e82b718f03a49871f32cd30481f6
yaroslavff <apreylg@yandex.ru>
2021-03-13 15:34:06 +0300

    Доделан switch с помощью JS и CSS анимации.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import "jquery";
 import "./interface/libs/jquery.maskedinput.min";
 
+// скрипты для input data
 import "./interface/form_elements/inputs/input";
 
+// скрипты для switch button
+import "./interface/form_elements/toggles/toggle";
 
-// import "./components/common";
-
-// import "./components/testjquery";
-
+// подключение стилей
 import "./main.scss";
 
 
