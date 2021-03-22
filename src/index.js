8632760a87829d6bc1f904c2aa76717d4235ec80
yaroslavff <apreylg@yandex.ru>
2021-03-22 22:01:59 +0300

    Сверстаны все основные кнопки

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./interface/libs/jquery.maskedinput.min";
 // скрипты для input data
 import "./interface/form_elements/inputs/input";
 
-// скрипты для switch button
+// скрипты для switch и like buttons
 import "./interface/form_elements/toggles/toggle";
 
 // подключение стилей
