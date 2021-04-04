d7bfdb530cc6ed6dad6055e177c9ff7d1545ed09
yaroslavff <apreylg@yandex.ru>
2021-04-04 17:06:36 +0300

    Сверстан expanded checkbox, внесены изменения в arrow__list

--- src/interface/form_elements/toggles/toggle.js
+++ src/interface/form_elements/toggles/toggle.js
@@ -76,4 +76,17 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 
+    // checkboxDropdawn
+
+    const checkDropdawnList = document.querySelector(`[data-checkbox-panel="open"]`);
+    const checkboxPanel = checkDropdawnList.querySelector(".checkbox__wrapper");
+    const checkArrowBox = checkDropdawnList.querySelector(".arrow");
+    const checkArrow = checkArrowBox.querySelector(".arrow__list");
+
+    checkArrowBox.addEventListener("click", () => {
+        
+        checkboxPanel.classList.toggle("hidden-content");
+        checkArrow.classList.toggle("arrow__list_open");
+    });
+
 });
